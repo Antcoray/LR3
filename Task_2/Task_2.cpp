80191b7c4b02594de10442c8c81a301a2354b0df
anton <antonio.kolesnikow@gmail.com>
2024-10-11 12:22:55 +0300

    correct input

--- Task_2/Task_2.cpp
+++ Task_2/Task_2.cpp
@@ -1,35 +1,72 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 double countN() {
-    double N = 0;
-    double dn = 0;
-    int n = 1;
-    do {
-        dn = 1 / pow(2, n) + 1 / pow(3, n);
-        N = N + dn;
-        ++n;
-    } while (dn >= 0.001);
-    return N;
+  double N = 0;
+  double dn = 0;
+  int n = 1;
+  do {
+    dn = 1 / pow(2, n) + 1 / pow(3, n);
+    N = N + dn;
+    ++n;
+  } while (dn >= 0.001);
+  return N;
 }
 
 void intro() {
-    std::cout << "Задание 2. Выполнил Колесников Антон Сергеевич" << std::endl;
-    std::cout << "Введите 1 чтобы найти сумму ряда с точностью ε=10^-3, общий член которого dn = 1/2^n + 1/3^n" << std::endl;
-    std::cout << "Введите 0 чтобы выйти" << std::endl;
+  std::cout << "Задание 2. Выполнил Колесников Антон Сергеевич" << std::endl;
+  std::cout << "Введите 1 чтобы найти сумму ряда с точностью ε=10^-3, общий "
+               "член которого dn = 1/2^n + 1/3^n"
+            << std::endl;
+  std::cout << "Введите 0 чтобы выйти" << std::endl;
 }
 
+int correctInput() {
+  bool incorrect = false;
+  bool error = false;
+  int s;
+  do {
+    incorrect = false;
+    std::string str = "";
+    std::cin >> str;
+    try {
+      s = std::stoi(str);
+    } catch (std::invalid_argument) {
+      std::cout
+          << "=== Некорректный ввод. Введите 1 чтобы найти сумму ряда или 0 "
+             "чтобы выйти ==="
+          << std::endl;
+      incorrect = true;
+      error = true;
+    } catch (std::out_of_range) {
+      std::cout
+          << "=== Некорректный ввод. Введите 1 чтобы найти сумму ряда или 0 "
+             "чтобы выйти ==="
+          << std::endl;
+      incorrect = true;
+      error = true;
+    }
+    if (s != 1 && s != 0 && error == false) {
+      std::cout
+          << "=== Некорректный ввод. Введите 1 чтобы найти сумму ряда или 0 "
+             "чтобы выйти ==="
+          << std::endl;
+
+      incorrect = true;
+    }
+  } while (incorrect);
+  return s;
+}
 int main() {
-    int s = 0;
-    intro();
-    while(true) {
-        std::cin >> s; 
+  intro();
+  while (true) {
+    int s = correctInput();
     if (s == 1) {
-        std::cout << "N = " << countN() << std::endl;
+      std::cout << "N = " << countN() << std::endl;
     }
     if (s == 0) {
-        break;
+      break;
     }
-    }
-    return 0;
+  }
+  return 0;
 }
